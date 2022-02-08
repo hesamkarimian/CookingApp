4313ce44a5d56e107b0d2224eae7dbefba5f3b8b
Hesam.Karimian <hessam.karimian@gmail.com>
2022-02-08 10:34:02 +0100

    add main controller and service classes

---
+++ src/main/java/com/sam/cookingapp/services/impl/RecipeIngredientServiceImpl.java
@@ -0,0 +1,62 @@
+package com.sam.cookingapp.services.impl;
+
+import com.sam.cookingapp.dto.IngredientDto;
+import com.sam.cookingapp.dto.RecipeDto;
+import com.sam.cookingapp.entitis.Recipe;
+import com.sam.cookingapp.exceptions.RecipeNotFoundException;
+import com.sam.cookingapp.mappers.RecipeMapper;
+import com.sam.cookingapp.repositories.RecipeIngredientRepository;
+import com.sam.cookingapp.repositories.RecipeRepository;
+import com.sam.cookingapp.services.RecipeIngredientService;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.mapstruct.factory.Mappers;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author HesamKarimian
+ * @since 20/01/2022
+ */
+
+@Service
+public class RecipeIngredientServiceImpl implements RecipeIngredientService {
+
+  private static final RecipeMapper RECIPE_MAPPER = Mappers.getMapper(RecipeMapper.class);
+  private final RecipeRepository recipeRepository;
+  private final RecipeIngredientRepository recipeIngredientRepository;
+
+  public RecipeIngredientServiceImpl(RecipeRepository recipeRepository,
+                                     RecipeIngredientRepository recipeIngredientRepository) {
+    this.recipeRepository = recipeRepository;
+    this.recipeIngredientRepository = recipeIngredientRepository;
+  }
+
+  @Override
+  public List<RecipeDto> getAllRecipes() {
+    return recipeRepository
+      .findAll()
+      .stream()
+      .map(RECIPE_MAPPER::toDto)
+      .collect(Collectors.toList());
+  }
+
+  @Override
+  public RecipeDto getById(Long id) {
+    Recipe recipe = recipeRepository.findById(id)
+                                    .orElseThrow(() -> new RecipeNotFoundException(id));
+    return RECIPE_MAPPER.toDto(recipe);
+  }
+
+  @Override
+  public RecipeDto addRecipe(RecipeDto dto) {
+    Recipe recipe = RECIPE_MAPPER.toEntity(dto);
+    recipeRepository.save(recipe);
+
+    return RECIPE_MAPPER.toDto(recipe);
+  }
+
+  @Override
+  public List<IngredientDto> getRecipeIngredient(RecipeDto dto) {
+    return null;
+  }
+}
