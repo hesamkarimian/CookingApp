5a2e032f6b6dae27eb4f7d49d5f80313181b5e9b
Hesam Karimian <hesam.karimian@kontron.com>
2022-11-16 19:57:24 +0100

    Add Spring Cloud Config and some initial data in DB
    Signed-off-by: Hesam Karimian <hesam.karimian@kontron.com>

---
+++ src/main/resources/db/changelog/changes/0004_Insert_Recipe_Data.sql
@@ -0,0 +1,5 @@
+truncate table recipes;
+INSERT INTO recipes(name, source_country, description) VALUES ('Ghorme Sabzi', 'Iran', 'Most Favorite Men dish');
+INSERT INTO recipes(name, source_country, description) VALUES ('Khoresh Gheime', 'Iran', 'Most Favorite One during Ashora');
+INSERT INTO recipes(name, source_country, description) VALUES ('Wiener Schnitzel', 'Austria', 'Most Famous cooked by Beef, Pork, Chicken or Turkey');
+INSERT INTO recipes(name, source_country, description) VALUES ('Wiener Schnitzel', 'Austria', 'Most Famous cooked by Beef, Pork, Chicken or Turkey');
